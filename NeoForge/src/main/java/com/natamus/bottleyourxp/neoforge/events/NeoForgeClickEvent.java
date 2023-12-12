bf1c9ac5ff39fcbe86975e01bd2f083fd1bf987d
Rick South <rick@natamus.com>
2023-12-12 10:50:16 +0100

    Bottle Your Xp source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/bottleyourxp/neoforge/events/NeoForgeClickEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.bottleyourxp.neoforge.events;
+
+import com.natamus.bottleyourxp.events.ClickEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeClickEvent {
+	@SubscribeEvent
+	public static void onClickBottle(PlayerInteractEvent.RightClickItem e) {
+		ClickEvent.onClickBottle(e.getEntity(), e.getLevel(), e.getHand());
+	}
+}
