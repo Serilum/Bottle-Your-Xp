296e389cd29a15bdcc9042deec115ddd018ad9d8
Rick South <rick@natamus.com>
2024-07-28 07:41:36 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/bottleyourxp/ModFabric.java
+++ Fabric/src/main/java/com/natamus/bottleyourxp/ModFabric.java
@@ -3,6 +3,7 @@ package com.natamus.bottleyourxp;
 import com.natamus.bottleyourxp.events.ClickEvent;
 import com.natamus.bottleyourxp.util.Reference;
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
 
@@ -10,6 +11,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
