785418526915825428669ade582c73999b2a2490
Rick South <rick@natamus.com>
2025-02-12 23:43:02 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/bottleyourxp/ModForge.java
+++ Forge/src/main/java/com/natamus/bottleyourxp/ModForge.java
@@ -32,7 +32,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-    	MinecraftForge.EVENT_BUS.register(new ForgeClickEvent());
+    	MinecraftForge.EVENT_BUS.register(ForgeClickEvent.class);
 	}
 
 	private static void setGlobalConstants() {
--- NeoForge/src/main/java/com/natamus/bottleyourxp/neoforge/events/NeoForgeClickEvent.java
+++ NeoForge/src/main/java/com/natamus/bottleyourxp/neoforge/events/NeoForgeClickEvent.java
@@ -3,9 +3,7 @@ package com.natamus.bottleyourxp.neoforge.events;
 import com.natamus.bottleyourxp.events.ClickEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeClickEvent {
 	@SubscribeEvent
 	public static void onClickBottle(PlayerInteractEvent.RightClickItem e) {
