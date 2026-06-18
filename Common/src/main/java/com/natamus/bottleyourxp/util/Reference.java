a6d1e41760a320401923ad4a9b7493d8a4e503ad
Rick South <rick@natamus.com>
2026-06-19 00:09:38 +0200

    Updated Bottle Your Xp to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/bottleyourxp/util/Reference.java
+++ Common/src/main/java/com/natamus/bottleyourxp/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "bottleyourxp";
 	public static final String NAME = "Bottle Your Xp";
 	public static final String VERSION = "3.5";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
